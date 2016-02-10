bda8723294f253c9dc4c600cd3d01f91ba34bf9b
Kapil <kapils2@damcogroup.com>
2016-02-10 20:22:48 +0530

    base scripts

---
+++ install_ruby.sh
@@ -0,0 +1,13 @@
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(rbenv init -)"' >> ~/.zprofile
+
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zprofile
+source ~/.zprofile
+
+rbenv install -v 2.3.0
+rbenv global 2.3.0
